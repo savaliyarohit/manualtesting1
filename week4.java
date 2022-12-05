af6985542cd3d4cf73ab9f9b38f6039eed15f310
savaliyarohit <savaliyarohit333@gmail.com>
2022-12-05 22:41:54 +0000

    Manualtesting1

---
+++ week4.java
@@ -0,0 +1,20 @@
+package Manualtesting1;
+
+public class week4 {
+
+    public static void main(String[] args) {
+    System.out.println("- Static Vs Dynamic Testing");
+    System.out.println("- Exploratory Testing");
+    System.out.println("- Ad-Hoc Testing");
+    System.out.println("- Software Development Models");
+    System.out.println("         * Waterfall");
+    System.out.println("         * Iterative");
+    System.out.println("         * V-Model");
+    System.out.println("         * Verification and Validation");
+    System.out.println("         * Agile");
+    System.out.println("         * Definition of Ready/Done");
+
+
+
+    }
+}
---
+++ week5.java
@@ -0,0 +1,14 @@
+package Manualtesting1;
+
+public class week5  {
+
+    public static void main(String[] args) {
+        System.out.println("- What is TDD?");
+        System.out.println("- What is BDD?");
+        System.out.println("       * BDD keywords");
+        System.out.println("       * How to Write test cases with BDD?");
+        System.out.println("- Maintenance");
+
+
+    }
+}
